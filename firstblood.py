aab8903751d1f37933388c7c2ab3112146dbe010
jDally987 <jdally987@gmail.com>
2019-04-27 06:35:30 -0400

    Capped number of echo repeats at 20x Added a couple methods in firstblood, notably: echo(): echoes back your message by a specified number of times

--- firstblood.py
+++ firstblood.py
@@ -63,6 +63,8 @@ def jeremyisa():
 def echo(t):
     indexofx = t.find('x')
     multiplier = int(t[5:indexofx])
+    if multiplier > 20:
+        multiplier = 20     # cap the number of repeats at 20x
     text = t[indexofx+2:]
     for i in range(multiplier):
         c.makeCall('bots', 'Post', text=text)
